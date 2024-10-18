123306085fac675c372818ff61af0fe85d7e0fdd
Siddharth Kar <35924946+siddkar@users.noreply.github.com>
2024-10-18 16:46:07 +0530

    updated

--- scripts/sst_deploy.sh
+++ scripts/sst_deploy.sh
@@ -3,7 +3,7 @@ echo "deploying applciation"
 ls
 npm install
 export SST_STAGE=sst-dev
-npx sst deploy
+npx sst deploy --verbose
 if [ "$CODEBUILD_BUILD_SUCCEEDING" == "0" ]; then
     cat .sst/log/sst.log
 fi
