2ccdd39c3f81b8247e4a67bab30ec40e4ae2c0e6
Siddharth Kar <siddharth.kar@outlook.com>
2024-10-22 19:52:17 +0530

    updated deploy script

--- .pipeline/scripts/deploy.sh
+++ .pipeline/scripts/deploy.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 echo "Deploying Application"
 
-echo "Environment: $ENVIRONMENT, CODEBUILD_WEBHOOK_HEAD_REF: ${CODEBUILD_WEBHOOK_HEAD_REF}"
+echo "Environment: $ENVIRONMENT"
+
+# Get the branch name from the source artifact
+BRANCH_NAME=$(aws codepipeline get-pipeline-state --name SSTV3DemoPipeline \
+| jq -r '.stageStates[] | select(.stageName == "Source") | .actionStates[0].latestExecution.revisionUrl' \
+| grep -o 'refs/heads/.*')
+
+echo "Branch Name: $BRANCH_NAME"
+
+
+# Conditionally execute based on branch
+if [ "$BRANCH_NAME" == "refs/heads/main" ]; then
+    echo "On main branch, proceeding with commands..."
+    # Run your AWS CLI commands or other scripts here
+    aws s3 ls
+else
+    echo "Not on main branch, skipping this action."
+    exit 0  # Exit to skip the remaining commands
+fi
 
 ls
 
-printenv
+# printenv
 
-npm install
+# npm install
 
-export SST_STAGE=$ENVIRONMENT
+# export SST_STAGE=$ENVIRONMENT
 
-echo "SST Stage = $SST_STAGE"
+# echo "SST Stage = $SST_STAGE"
 
 # npx sst deploy --print-logs
 
