c4381caf6227c0c8d4392acba989dead855e4274
Siddharth Kar <siddharth.kar@outlook.com>
2024-10-22 20:46:21 +0530

    updated deploy pipeline

--- .pipeline/scripts/unit-test.sh
+++ .pipeline/scripts/unit-test.sh
@@ -9,4 +9,11 @@ npm install
 
 npm run test:coverage
 
+BRANCH_NAME=$(aws codepipeline get-pipeline-state --name SSTV3DemoPipeline --query 'stageStates[].actions[].inputArtifacts[].revision' --output text)
+
+# Print the branch name
+echo "$BRANCH_NAME"
+
+echo " BRANCH_NAME >>> $BRANCH_NAME "
+
 exit 0
