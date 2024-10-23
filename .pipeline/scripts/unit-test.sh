dac6f0fcd27501ff8478ba6bdbdc8a68977e4c52
Siddharth Kar <siddharth.kar@outlook.com>
2024-10-23 09:10:53 +0530

    added echo

--- .pipeline/scripts/unit-test.sh
+++ .pipeline/scripts/unit-test.sh
@@ -3,17 +3,12 @@ echo "Running Unit Tests"
 
 echo "Environment: $ENVIRONMENT"
 
+echo pipeline id #{codepipeline.PipelineExecutionId}
+
 ls
 
 npm install
 
 npm run test:coverage
-
-BRANCH_NAME=$(aws codepipeline get-pipeline-state --name SSTV3DemoPipeline --query 'stageStates[].actions[].inputArtifacts[].revision' --output text)
-
-# Print the branch name
-echo "$BRANCH_NAME"
-
-echo " BRANCH_NAME >>> $BRANCH_NAME "
 
 exit 0
